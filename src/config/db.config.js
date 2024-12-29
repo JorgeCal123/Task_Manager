6a1e0e42f3f2c7e34d78b2e667c68bcde98db464
jorgecal123 <ingsistemas68@gmail.com>
2024-12-29 18:50:35 -0500

    modificacion en la connexion a mongodb por defecto

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const mongouri = "mongodb+srv://databasejc:erRooPddZZad1tPM@cluster0.h3pdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    //const mongouri = process.env.MONGODB_URI || 'mongodb://localhost:27017/task-manager';
+    console.log('Iniciando conexión a MongoDB...');
+
     if (!mongouri) {
       throw new Error('No se puede hacer la conexion a la Url ');
     }
     
+    await mongoose.connect(mongouri);
     console.log('Conexión a MongoDB establecida');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error.message);
